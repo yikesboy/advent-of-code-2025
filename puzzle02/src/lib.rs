02a78c74ce0733178933c1515500a29b31269657
JL <115737916+yikesboy@users.noreply.github.com>
2025-12-02 22:00:32 +0100

    puzzle2 part2

--- puzzle02/src/lib.rs
+++ puzzle02/src/lib.rs
@@ -29,5 +29,35 @@ pub fn part1(input: &str) -> i64 {
 }
 
 pub fn part2(input: &str) -> i64 {
-    0
+    let range_strs: Vec<&str> = input.split(',').collect();
+    let mut sum_invalid_ids = 0;
+
+    for range in range_strs {
+        let (range_start, range_end) = range.split_once('-').expect("should be just one -");
+        let range_start: i64 = range_start.trim().parse().expect("Invalid range start.");
+        let range_end: i64 = range_end.trim().parse().expect("Invalid range end.");
+
+        for i in range_start..=range_end {
+            let current = i.to_string();
+            let len = current.chars().count();
+
+            for period in 1..=len / 2 {
+                let pattern = &current.as_str()[..period];
+                let mut is_repeated = true;
+
+                for i in (period..len).step_by(period) {
+                    if i + period > len || &current[i..i + period] != pattern {
+                        is_repeated = false;
+                        break;
+                    }
+                }
+
+                if is_repeated && len % period == 0 {
+                    sum_invalid_ids += i;
+                    break;
+                }
+            }
+        }
+    }
+    return sum_invalid_ids;
 }
--- puzzle02/tests/test.rs
+++ puzzle02/tests/test.rs
@@ -14,6 +14,12 @@ fn test_part1_input() {
 
 #[test]
 fn test_part2_example() {
-    let input = "";
-    assert_eq!(part2(input), 0);
+    let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+    assert_eq!(part2(input), 4174379265);
+}
+
+#[test]
+fn test_part2_input() {
+    let input = include_str!("../src/input.txt");
+    assert_eq!(part2(input), 40028128307);
 }
