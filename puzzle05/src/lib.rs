b35ac36e32eb670e1f270f35391dca130f0b948d
JL <115737916+yikesboy@users.noreply.github.com>
2025-12-05 21:04:52 +0100

    puzzle5 part 2

--- puzzle05/src/lib.rs
+++ puzzle05/src/lib.rs
@@ -33,5 +33,38 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i64 {
-    0
+    let parts = input.split_once("\n\n").expect("Should contain two parts");
+
+    let mut fresh_ingredient_ranges: Vec<(i64, i64)> = parts
+        .0
+        .lines()
+        .map(|line| line.split_once("-").unzip())
+        .map(|tuple| {
+            (
+                tuple.0.unwrap().parse::<i64>().expect("should be num"),
+                tuple.1.unwrap().parse::<i64>().expect("should be num"),
+            )
+        })
+        .collect();
+
+    fresh_ingredient_ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut merged = vec![fresh_ingredient_ranges[0]];
+    for (start, end) in fresh_ingredient_ranges {
+        let last_idx = merged.len() - 1;
+        let (last_start, last_end) = merged[last_idx];
+
+        if start <= last_end {
+            merged[last_idx] = (last_start, last_end.max(end))
+        } else {
+            merged.push((start, end));
+        }
+    }
+
+    let mut result = 0;
+    for (start, end) in merged {
+        result += end - start + 1
+    }
+
+    return result;
 }
--- puzzle05/tests/test.rs
+++ puzzle05/tests/test.rs
@@ -24,6 +24,22 @@ fn test_part1_input() {
 
 #[test]
 fn test_part2_example() {
-    let input = "";
-    assert_eq!(part2(input), 0);
+    let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+    assert_eq!(part2(input), 14);
+}
+
+#[test]
+fn test_part2_input() {
+    let input = include_str!("../src/input.txt");
+    assert_eq!(part2(input), 357485433193284);
 }
